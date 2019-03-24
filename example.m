8a14ded85aeb4ce05adf392a8ff1e978b64fd7de
Kate <41831314+Very-Fancy@users.noreply.github.com>
2019-03-24 23:16:18 +0300

    Update example.m

--- example.m
+++ example.m
@@ -1,7 +1,10 @@
 
 %   Creating McEliece System with default parameters
-mc_mdpc = mceliece();
-
+mc_mdpc = mceliece(); 
+%mceliece(0, t, params); % 0 for mdpc, t: number of errors for
+%   the encryption error vector, params: [n0, p, w], for now n0=2 is required, 
+%   p - prime
+%
 %   Parameters
 [k n] = mc_mdpc.get_params();
 
@@ -16,3 +19,4 @@ disp(sprintf('x = %s\n', dec2bin(x)'));
 %   Message Decryption
 y = mc_mdpc.decrypt(x);
 disp(sprintf('y = %s\n', dec2bin(y)'));
+
